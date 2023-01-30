b7aac4b436333b5b88d3b5c7e9615ace69697f2c
dcaaz <danicorreaazevedo@gmail.com>
2023-01-30 17:16:58 -0300

    refactor: seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,34 +1,37 @@
-// import prisma from "../src/database/db";
+   import prisma from "../src/database/db.js";
 
-// async function main() {
-//     await prisma.order.createMany({
-//         data: [
-//             {
-//                 "item": "milho",
-//                 "name": "Davi",
-//                 "phonetype": 48996395282,
-//                 "value": 5,
-//                 "referencepoint": "perto da barraca de caipirinha"
-//             },
-//             {
-//                 "item": "água de coco",
-//                 "name": "Dani",
-//                 "phonetype": 48998215536,
-//                 "value": 10,
-//                 "referencepoint": "perto da entrada da praia"
-//             }
-//         ]
-//     })
-// }
+   async function main() {
+       await prisma.order.createMany({
+           data: [
+               {
+                    "item": "Açai", 
+                    "name": "Shiva", 
+                    "phonetype": 48996385290, 
+                    "value": 5, 
+                    "referencepoint": "perto da barraca de caipirinha",
+                    "category": 2
+               },
+               {
+                   "item": "água de coco",
+                   "name": "Dani",
+                   "phonetype": 48998215589,
+                   "value": 10,
+                   "referencepoint": "perto da entrada da praia",
+                   "category": 1
+               }
+           ]
+       })
+   }
 
-// main()
-// .then(() => {
-//     console.log("Registro feito com sucesso!")
-// })
-// .catch(e => {
-//     console.error(e);
-//     process.exit(1);
-// })
-// .finally(async () => {
-//    await prisma.$disconnect();
-// })
+   main()
+   .then(() => {
+       console.log("Registro feito com sucesso!")
+   })
+   .catch(e => {
+       console.error(e);
+       process.exit(1);
+   })
+   .finally(async () => { //encerrar conexão/desconectar do banco caso
+    //aconteça algum erro, vazamento de dados
+      await prisma.$disconnect();
+   })
